b492a62d1bad087b72f9a1214c380150b99e59d8
Josef <josef@boukdir.se>
2024-07-06 16:15:04 +0200

    fileserver handle added and a placeholder index.html

--- internal/server/server.go
+++ internal/server/server.go
@@ -24,14 +24,16 @@ func NewServer(listenAddr string, router *http.ServeMux, db *sql.DB) *Server {
 
 func (s *Server) setupRoutes() {
 	subrouter := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./web/templates"))
 
 	subrouter.HandleFunc("GET /healthz", handlers.HealthCheck)
 
+	s.router.Handle("/app/", http.StripPrefix("/app", fs))
 	s.router.Handle("/v1/", http.StripPrefix("/v1", subrouter))
 }
 
 func (s *Server) Run() error {
 	s.setupRoutes()
-	log.Printf("Server starting and listening on :%s", s.listenAddr)
+	log.Printf("Server starting and listening on %s", s.listenAddr)
 	return http.ListenAndServe(s.listenAddr, s.router)
 }
