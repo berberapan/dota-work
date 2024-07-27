fce952818ea43f589d35e54ac3f56fd36905d269
Josef <josef@boukdir.se>
2024-07-27 20:20:13 +0200

    refactored big func to smaller ones

--- internal/fetcher/process_liquipedia.go
+++ internal/fetcher/process_liquipedia.go
@@ -2,6 +2,7 @@ package fetcher
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -9,6 +10,13 @@ import (
 
 	"github.com/berberapan/dota-work/internal/utils"
 )
+
+const (
+	matchIndicator = "{{Match2"
+	ukTimeZone     = "Europe/London"
+)
+
+var defaultTime time.Time = time.Date(2020, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
 
 func LiquipediaDataToMap(url string) map[string]interface{} {
 	var result map[string]interface{}
@@ -34,103 +42,149 @@ func LiquipediaMappedDataToString(data map[string]interface{}) string {
 }
 
 func GetScheduleOfTournament(url, leagueCode string) []MatchScheduleData {
-	schedule := []MatchScheduleData{}
 	mapData := LiquipediaDataToMap(url)
 	data := LiquipediaMappedDataToString(mapData)
+	lines := strings.Split(data, "\n")
 	leagueCodeInt, _ := strconv.Atoi(leagueCode)
 
-	lines := strings.Split(data, "\n")
+	var schedule []MatchScheduleData
+	var currentMatch matchInfo
+	tournamentName := extractTournamentName(lines)
 	isMatch := false
-
-	bestof := 0
-	teamA := ""
-	teamB := ""
-	date := ""
-	startTime := ""
-	lc := leagueCodeInt
-	tournamentName := ""
-	game := "DOTA2"
+	bestOf := 0
 
 	for _, line := range lines {
-		if strings.Contains(line, "tickername") {
-			tournamentName = line[strings.Index(line, "=")+1:]
-		}
-		if strings.Contains(line, "{{Match2") {
+		if strings.Contains(line, matchIndicator) {
 			isMatch = true
+			currentMatch = matchInfo{prevBestOf: bestOf}
 		}
-		if isMatch && strings.Contains(line, "bestof") {
-			bestOfString := line[strings.Index(line, "=")+1:]
-			bestof, _ = strconv.Atoi(bestOfString)
-		}
-		if isMatch && strings.Contains(line, "opponent1") {
-			startIdx := strings.LastIndex(line, "|") + 1
-			endIdx := strings.Index(line, "}}")
-			teamA = line[startIdx:endIdx]
-		}
-		if isMatch && strings.Contains(line, "opponent2") {
-			startIdx := strings.LastIndex(line, "|") + 1
-			endIdx := strings.Index(line, "}}")
-			teamB = line[startIdx:endIdx]
-		}
-		if isMatch && strings.Contains(line, "date") {
-			dateLine := line[strings.Index(line, "=")+1:]
-			dateLineClean := strings.ReplaceAll(dateLine, ",", "")
-			objects := strings.Fields(dateLineClean)
-			srcYear, err := strconv.Atoi(objects[2])
-			if err != nil {
-				log.Printf("Unable to convert string Year to int Year.\n%v", err)
-				return schedule
+		if isMatch {
+			currentMatch.updateFromLine(line)
+			if currentMatch.isComplete() {
+				schedule = append(schedule, createMatchScheduleData(currentMatch, tournamentName, leagueCodeInt))
+				isMatch = false
+				bestOf = currentMatch.bestOf
 			}
-			srcMonth := utils.ConvertStringToMonth(objects[0])
-			srcDay, err := strconv.Atoi(objects[1])
-			if err != nil {
-				log.Printf("Unable to convert string Day to int Day.\n%v", err)
-				return schedule
-			}
-			hhMM := strings.Split(objects[4], ":")
-			srcHour, err := strconv.Atoi(hhMM[0])
-			if err != nil {
-				log.Printf("Unable to convert string Hour to int Hour.\n%v", err)
-				return schedule
-			}
-			srcMin, err := strconv.Atoi(hhMM[1])
-			if err != nil {
-				log.Printf("Unable to convert string Hour to int Hour.\n%v", err)
-				return schedule
-			}
-			tzObject := objects[5]
-			tzAbbr := tzObject[strings.Index(tzObject, "/")+1 : strings.Index(tzObject, "}}")]
-			srcLoc := utils.ConvertTzAbbrToTzString(tzAbbr)
-			location, err := time.LoadLocation(srcLoc)
-			if err != nil {
-				log.Printf("Unable to load source timezone.\n%v", err)
-				return schedule
-			}
-			ukTime, err := time.LoadLocation("Europe/London")
-			if err != nil {
-				log.Printf("Unable to load UK time.\n%v", err)
-				return schedule
-			}
-
-			srcTime := time.Date(srcYear, srcMonth, srcDay, srcHour, srcMin, 0, 0, location)
-			convertedTime := srcTime.In(ukTime)
-
-			date = utils.ConvertDateToString(convertedTime)
-			startTime = utils.ConvertTimeToString(convertedTime)
-
-			match := MatchScheduleData{
-				TeamA:      teamA,
-				TeamB:      teamB,
-				Tournament: tournamentName,
-				BestOf:     bestof,
-				LeagueCode: lc,
-				Date:       date,
-				StartTime:  startTime,
-				Game:       game,
-			}
-			schedule = append(schedule, match)
-			isMatch = false
 		}
 	}
 	return schedule
+}
+
+type matchInfo struct {
+	bestOf     int
+	prevBestOf int
+	teamA      string
+	teamB      string
+	dateTime   time.Time
+}
+
+func (m *matchInfo) updateFromLine(line string) {
+	switch {
+	case strings.Contains(line, "bestof"):
+		m.bestOf, _ = extractIntValue(line)
+	case strings.Contains(line, "opponent1"):
+		m.teamA = extractTeamName(line)
+	case strings.Contains(line, "opponent2"):
+		m.teamB = extractTeamName(line)
+	case strings.Contains(line, "|date"):
+		m.dateTime = extractDateTime(line)
+	}
+}
+
+func (m *matchInfo) isComplete() bool {
+	if m.bestOf == 0 && m.teamA != "" && m.teamB != "" && !m.dateTime.IsZero() {
+		m.bestOf = m.prevBestOf
+	}
+	return m.bestOf > 0 && m.teamA != "" && m.teamB != "" && !m.dateTime.IsZero()
+}
+
+func extractTeamName(line string) string {
+	matchLine := line[1:]
+	startIdx := strings.Index(matchLine, "|") + 1
+	endIdx := strings.Index(matchLine, "}}")
+	teamName := strings.TrimSpace(matchLine[startIdx:endIdx])
+	if teamName == "" {
+		teamName = "TBA"
+	}
+	return teamName
+}
+
+func extractIntValue(line string) (int, error) {
+	valueStr := strings.TrimSpace(line[strings.Index(line, "=")+1:])
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to convert string to int.\n%w", err)
+	}
+	return value, nil
+}
+
+func extractTournamentName(lines []string) string {
+	for _, line := range lines {
+		if strings.Contains(line, "tickername") {
+			return strings.TrimSpace(line[strings.Index(line, "=")+1:])
+		}
+	}
+	return ""
+}
+
+func extractDateTime(line string) time.Time {
+	dateLine := strings.TrimSpace(line[strings.Index(line, "=")+1:])
+	dateLineClean := strings.ReplaceAll(dateLine, ",", "")
+	objects := strings.Fields(dateLineClean)
+	if len(objects) == 0 {
+		return defaultTime
+	}
+	srcYear, err := strconv.Atoi(objects[2])
+	if err != nil {
+		log.Printf("Unable to convert string Year to int Year.\n%v", err)
+		return time.Time{}
+	}
+	srcMonth := utils.ConvertStringToMonth(objects[0])
+	srcDay, err := strconv.Atoi(objects[1])
+	if err != nil {
+		log.Printf("Unable to convert string Day to int Day.\n%v", err)
+		return time.Time{}
+	}
+
+	hhMM := strings.Split(objects[4], ":")
+	srcHour, err := strconv.Atoi(hhMM[0])
+	if err != nil {
+		log.Printf("Unable to convert string Hour to int Hour.\n%v", err)
+		return time.Time{}
+	}
+	srcMin, err := strconv.Atoi(hhMM[1])
+	if err != nil {
+		log.Printf("Unable to convert string Minute to int Minute.\n%v", err)
+		return time.Time{}
+	}
+
+	tzObject := objects[5]
+	tzAbbr := tzObject[strings.Index(tzObject, "/")+1 : strings.Index(tzObject, "}}")]
+	srcLoc := utils.ConvertTzAbbrToTzString(tzAbbr)
+
+	location, err := time.LoadLocation(srcLoc)
+	if err != nil {
+		log.Printf("Unable to load source timezone. Setting UTC timezone.\n%v", err)
+		location = time.UTC
+	}
+	ukTime, err := time.LoadLocation(ukTimeZone)
+	if err != nil {
+		log.Printf("Unable to load UK timezone.\n%v", err)
+		return defaultTime
+	}
+	srcTime := time.Date(srcYear, srcMonth, srcDay, srcHour, srcMin, 0, 0, location)
+	return srcTime.In(ukTime)
+}
+
+func createMatchScheduleData(m matchInfo, tournamentName string, leagueCode int) MatchScheduleData {
+	return MatchScheduleData{
+		TeamA:      m.teamA,
+		TeamB:      m.teamB,
+		Tournament: tournamentName,
+		BestOf:     m.bestOf,
+		LeagueCode: leagueCode,
+		Date:       utils.ConvertDateToString(m.dateTime),
+		StartTime:  utils.ConvertTimeToString(m.dateTime),
+		Game:       "DOTA2",
+	}
 }
